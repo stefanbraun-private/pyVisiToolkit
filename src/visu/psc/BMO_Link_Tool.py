2eb5a94cc56c33f3f6c6fafc3fc22138588f77e3
Stefan Braun <stefanbraun-private@users.noreply.github.com>
2018-04-18 21:11:39 +0200

    work in progress...

--- src/visu/psc/BMO_Link_Tool.py
+++ src/visu/psc/BMO_Link_Tool.py
@@ -60,6 +60,26 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 
+class BMO_Instance_Metadata(object):
+	"""
+	metadata per BMO instance
+	"""
+	def __init__(self):
+		self._max_coords = 0, 0, 0, 0
+		self._bmo_class = ''
+
+	def set_BMO_class(self, bmo_class):
+		self._bmo_class = str(bmo_class)
+
+	def get_BMO_class(self):
+		return self._bmo_class
+
+	def set_max_coords(self, *coords):
+		self._max_coords = tuple(coords)
+
+	def get_max_coords(self):
+		return self._max_coords
+
 
 class BMO_Elements_Handler(object):
 	"""
@@ -68,9 +88,7 @@ class BMO_Elements_Handler(object):
 	def __init__(self):
 		self.fname = None
 
-		# maximum size for drawing BMO instances
-		self._bmo_sizes = {}
-		self._bmo_classes = {}
+		self._bmo_instances = {}
 
 	def set_psc_file(self, fname):
 		self.fname = fname
@@ -95,39 +113,38 @@ class BMO_Elements_Handler(object):
 
 
 	def draw_elements(self, canvas_visu):
-		self._bmo_sizes = {}
-		self._bmo_classes = {}
+		self._bmo_instances = {}
 
 		# collect information for draw this element
 		for elem, instance, bmo_class in self._filtered_elements():
 			coords = elem.get_property(u'selection-area').get_coordinates()
 			try:
 				# FIXME: write these comparisons in a cleaner way
-				_x1, _y1, _x2, _y2 = self._bmo_sizes[instance]
+				_x1, _y1, _x2, _y2 = self._bmo_instances[instance].get_max_coords()
 				x1 = coords[0] if coords[0] < _x1 else _x1
 				x2 = coords[2] if coords[2] > _x2 else _x2
 				y1 = coords[1] if coords[1] < _y1 else _y1
 				y2 = coords[3] if coords[3] > _y2 else _y2
-				self._bmo_sizes[instance] = x1, y1, x2, y2
+				self._bmo_instances[instance].set_max_coords(x1, y1, x2, y2)
 			except KeyError:
 				# first appearance of this BMO instance
-				self._bmo_sizes[instance] = coords
+				self._bmo_instances[instance] = BMO_Instance_Metadata()
+				self._bmo_instances[instance].set_max_coords(*coords)
 
-			if not instance in self._bmo_classes:
-				self._bmo_classes[instance] = bmo_class
+			self._bmo_instances[instance].set_BMO_class(bmo_class)
 
 		# draw this list on main canvas
-		for key, coords in self._bmo_sizes.items():
-			logger.debug('draw_elements(): BMO instance "' + key + '" [' + self._bmo_classes[instance] + '] has maximum coordinates ' + repr(coords))
-			canvas_visu.create_rectangle(*coords, fill="white", outline="blue")
+		for key, curr_instance in self._bmo_instances.items():
+			logger.debug('draw_elements(): BMO instance "' + key + '" [' + self._bmo_instances[key].get_BMO_class() + '] has maximum coordinates ' + repr(curr_instance.get_max_coords()))
+			canvas_visu.create_rectangle(*curr_instance.get_max_coords(), fill="white", outline="blue")
 
 			# add text into rectangle
 			# help from https://stackoverflow.com/questions/39087139/tkinter-label-text-in-canvas-rectangle-python
 			# calculate center:
-			x1, y1, x2, y2 = coords
+			x1, y1, x2, y2 = curr_instance.get_max_coords()
 			x = (x1 + x2) / 2
 			y = (y1 + y2) / 2
-			canvas_visu.create_text((x, y), text=self._bmo_classes[instance])
+			canvas_visu.create_text((x, y), text=self._bmo_instances[key].get_BMO_class())
 
 
 
@@ -135,7 +152,8 @@ class MyGUI(Tkinter.Frame):
 	# example code from http://zetcode.com/gui/tkinter/layout/
 
 	ROOT_TITLE = u'BMO Link Tool v0.0.1'
-	def __init__(self, parent, psc_handler, dms, prj):
+
+	def __init__(self, parent, psc_handler, dms, prj, ge_host):
 		Tkinter.Frame.__init__(self, parent)
 		self.parent = parent
 		# settings of rootwindow
@@ -144,22 +162,17 @@ class MyGUI(Tkinter.Frame):
 		self.psc_handler = psc_handler
 		self._dms = dms
 		self._prj = prj
+		self._ge_host = ge_host
+		self._imgreinit = ''
 
-		self._buildFormular()
-		self._draw_frame = Tkinter.Frame(master=self)
+		self._canvas_frame = Tkinter.Frame(master=self)
 		self._canvas_visu = None
-		self._buildCanvasVisu(parent=self._draw_frame)
-		self._draw_frame.pack(fill=Tkconstants.BOTH, expand=True)
+		self._buildCanvasVisu(parent=self._canvas_frame)
+		self._canvas_frame.pack(fill=Tkconstants.BOTH, expand=True)
 		self.pack(fill=Tkconstants.BOTH, expand=True)
 
-	def _buildFormular(self):
 
-		self._frame_formular = Tkinter.Frame(master=self)
-
-		btn = Tkinter.Button(self._frame_formular, text='Select PSC file...', command=self._cb_button_selectfile)
-		btn.pack(fill=Tkconstants.BOTH, padx=5, pady=5)
-
-		self._frame_formular.pack(fill=Tkconstants.BOTH, padx=5, pady=5)
+		self._register_dms_callbacks()
 
 
 	def _buildCanvasVisu(self, parent):
@@ -173,31 +186,70 @@ class MyGUI(Tkinter.Frame):
 		self._canvas_visu.grid(row=0, column=1, padx=5)
 
 
-	def _cb_button_selectfile(self):
-		# file browser
-		# (example from http://tkinter.unpythonic.net/wiki/tkFileDialog )
+	def _register_dms_callbacks(self):
+		"""
+		monitoring PSC document opening activity in currently running GE instance
+		"""
 
-		# define options for opening or saving a file
-		file_opt = options = {}
-		options['defaultextension'] = '.psc'
-		options['filetypes'] = [('PSC files', '.psc'), ('all files', '.*')]
-		options['initialdir'] = os.path.join(self._prj, 'scr')
-		options['initialfile'] = ''
-		options['parent'] = self._frame_formular
-		options['title'] = 'Select PSC file for preview'
+		dms_key1_str = ':'.join(['System:Node', self._ge_host, 'ImgReInit'])
+		dms_key2_str = ':'.join(['System:Node', self._ge_host, 'Image'])
 
-		filename = tkFileDialog.askopenfilename(**file_opt)
-		if filename:
-			# settings of rootwindow
-			self.parent.title(MyGUI.ROOT_TITLE + u' - ' + filename)
+		for dms_key_str, callback in [(dms_key1_str, self._cb_ge_reinit_changed),
+		                              (dms_key2_str, self._cb_ge_image_changed)]:
+			logger.debug('MyGUI._register_dms_callback(): trying to subscribe DMS key "' + dms_key_str + '"...')
+			sub_obj = self._dms.get_dp_subscription(path=dms_key_str,
+			                                        event=dms.ON_SET)
+			logger.debug('MyGUI._register_dms_callback(): adding callback for DMS key "' + dms_key_str + '"...')
+			msg = sub_obj.sub_response.message
+			if not msg:
+				sub_obj += callback
 
-			# preset parser and load PSC image
-			self.psc_handler.set_psc_file(filename)
-			self._update_canvas()
+				# first run: use information we got from subscription
+				callback(None, sub_obj.sub_response.value)
+				logger.info('MyGUI._register_dms_callback(): monitoring of DMS key "' + dms_key_str + '" is ready.')
+			else:
+				logger.error('MyGUI._register_dms_callback(): monitoring of DMS key "' + dms_key_str + '" failed! [message: ' + msg + '])')
+				raise Exception('subscription failed!')
+
+
+	def _cb_ge_reinit_changed(self, event, *args):
+		""" another PSC file was opened in GE (instance of BMO) """
+		if args:
+			# first run
+			self._imgreinit = args[0]
+		else:
+			# assumption: called by DMS event
+			self._imgreinit = event.value
+
+
+	def _cb_ge_image_changed(self, event, *args):
+		""" another PSC file was opened in GE """
+		if args:
+			# first run
+			filename = args[0]
+		else:
+			# assumption: called by DMS event
+			filename = event.value
+
+		# only redraw canvas when shown PSC is not a reinit of BMO instance
+		if not self._imgreinit:
+			logger.debug('MyGUI._cb_ge_image_changed(): GE opened PSC file "' + filename + '"')
+			self._load_psc_image(filename)
+
+
+	def _load_psc_image(self, filename):
+		psc_fullpath = os.path.join(self._prj, 'scr', filename)
+
+		# settings of rootwindow
+		self.parent.title(MyGUI.ROOT_TITLE + u' - ' + psc_fullpath)
+
+		# preset parser and load PSC image
+		self.psc_handler.set_psc_file(psc_fullpath)
+		self._update_canvas()
 
 
 	def _update_canvas(self):
-		self._buildCanvasVisu(parent=self._draw_frame)
+		self._buildCanvasVisu(parent=self._canvas_frame)
 		self.psc_handler.draw_elements(canvas_visu=self._canvas_visu)
 
 
@@ -209,20 +261,30 @@ def main(dms_server, dms_port):
 	                                    dms_port_int=dms_port) as dms_ws:
 		logger.info('established WebSocket connection to DMS version ' + dms_ws.dp_get(path='System:Version:dms.exe')[0]['value'])
 
-		curr_proj = dms_ws.dp_get(path='System:Project')[0]['value']
+		curr_proj = dms_ws.dp_get(path='System:Project')[0].value
 		logger.info('current project is "' + curr_proj + '")')
 
-		# FIXME: implement a cleaner way for keeping ONE instance of ParserConfig in whole program...
-		Parser.PscParser.load_config(Parser.PARSERCONFIGFILE)
+		curr_ge_host = None
+		for resp in dms_ws.dp_get(path="", query=dms.Query(regExPath=r'System:Prog:GE:.*:UP', maxDepth=0)):
+			if resp.value:
+				curr_ge_host = resp.path.replace('System:Prog:GE:', '')[:-3]
+				logger.debug('found a running GE instance on host "' + curr_ge_host + '"...')
+				break
 
-		curr_psc_handler = BMO_Elements_Handler()
+		if curr_ge_host:
+			# FIXME: implement a cleaner way for keeping ONE instance of ParserConfig in whole program...
+			Parser.PscParser.load_config(Parser.PARSERCONFIGFILE)
 
-		# Build a gui
-		rootWindow = Tkinter.Tk()
-		app = MyGUI(parent=rootWindow, psc_handler=curr_psc_handler, dms=dms_ws, prj=curr_proj)
+			curr_psc_handler = BMO_Elements_Handler()
 
-		# Keeps GUI mainloop running until GUI is closed
-		rootWindow.mainloop()
+			# Build a gui
+			rootWindow = Tkinter.Tk()
+			app = MyGUI(parent=rootWindow, psc_handler=curr_psc_handler, dms=dms_ws, prj=curr_proj, ge_host=curr_ge_host)
+
+			# Keeps GUI mainloop running until GUI is closed
+			rootWindow.mainloop()
+		else:
+			logger.error('no local instance of GE found! GE is mandatory for using this tool...')
 
 	logger.info('Quitting "BMO_Link_Tool"...')
 
